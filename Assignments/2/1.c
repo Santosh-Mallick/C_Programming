b2c65aa78152249281ced64b194736156d90022c
Santosh Mallick <sant6225@gmail.com>
2023-12-17 23:37:23 +0530

    a2q1

---
+++ Assignments/2/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("\n Enter Number : ");
+    scanf("%d",&a);
+    printf("\n Enter Second Number : ");
+    scanf("%d",&b);
+    printf("\n Enter Third Number : ");
+    scanf("%d",&c);
+
+    if(a>=b && a>=c)
+    printf("%d is the Largest.",a);
+    else if(b>=a && b>=c)
+    printf("%d is the Largest.",b);
+    else 
+    printf("%d is the Largest.",c);
+    return 0;
+}
