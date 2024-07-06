ee52da1787c59532adc49fd8c309b06ce1d12524
Santosh-Mallick <sant6225@gmail.com>
2024-07-06 08:15:13 +0530

    4c

--- Assignments/1/4.C
+++ Assignments/1/4.C
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
     char ch;
     printf("\nDisplay ASCII Value Of Character \n\n");
     printf("\nEnter a Character : ");
     scanf("%c", &ch);
     printf("\nASCII Value Of The Character Is : %d", ch);
+    return 0;
 }
